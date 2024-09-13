d7dea7544d08841e530aa16e0767624156fac28a
Mochamad Rizky Ramadhan <mrizkyr2411@gmail.com>
2024-09-13 10:12:32 +0700

    first commit

---
+++ src/application/web.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+// import { publicRouter } from '../route/public-api';
+// import { errorMiddleware } from '../middleware/error-middleware';
+// import cookieParser from 'cookie-parser';
+import cors from 'cors';
+// import { authApi } from '../route/auth-api';
+
+export const web = express();
+web.use(express.json());
+web.use(express.static('public'));
+web.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+// web.use(cookieParser());
+
+// web.use(publicRouter);
+// web.use(authApi);
+
+// web.use(errorMiddleware);
+
+web.get('/test', (req, res) => {
+    res.send('Hello World');
+});
